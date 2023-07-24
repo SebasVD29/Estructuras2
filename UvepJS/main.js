31b563964a3e0f9e805174b4ddb73ab3574b95d4
SebasVD29 <100324444+SebasVD29@users.noreply.github.com>
2023-07-24 15:26:28 -0600

    JS

---
+++ UvepJS/main.js
@@ -0,0 +1,4 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2ViYXN2ZDI5IiwiYSI6ImNsa2JxYzAwaDBpYnIzcm54eHc2aHFwbTUifQ.zYhLbAM2_s9a5qZQX4FTqQ';var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+  });
