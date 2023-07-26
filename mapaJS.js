7522d81c5668cea74dec3aa2d5121f65fa253930
SebasVD29 <100324444+SebasVD29@users.noreply.github.com>
2023-07-26 15:58:00 -0600

    Se agrego el mapa mainpage

--- mapaJS.js
+++ mapaJS.js
@@ -1,9 +1,3 @@
-/*igual esto era con el mapbox 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2ViYXN2ZDI5IiwiYSI6ImNsa2JxYzAwaDBpYnIzcm54eHc2aHFwbTUifQ.zYhLbAM2_s9a5qZQX4FTqQ';var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-  });*/
-  
   var myLatLng = { lat: 10.213697085664514, lng: -83.7890126854106 };
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 9,
