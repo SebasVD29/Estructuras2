e72f5fdf1696a73e2e55bafd523c99f7643028eb
isabellaaaaa03 <100324470+isabellaaaaa03@users.noreply.github.com>
2023-08-18 23:49:49 -0600

    prim

--- prim.js
+++ prim.js
@@ -60,3 +60,87 @@ function primAlgorithm(nodes, edges) {
   return selectedEdges;
 }
 */
+function findClosestUnvisitedVertex(distances, visited) {
+  let minDistance = Number.MAX_VALUE;
+  let closestVertex = null;
+
+  for (const vertex in distances) {
+    if (!visited[vertex] && distances[vertex] < minDistance) {
+      minDistance = distances[vertex];
+      closestVertex = vertex;
+    }
+  }
+
+  return closestVertex;
+}
+
+function primAlgorithm(locations) {
+  const vertexCount = locations.length;
+  const distances = {};
+  const visited = {};
+
+  locations.forEach((location, index) => {
+    distances[index] = Number.MAX_VALUE;
+    visited[index] = false;
+  });
+
+  distances[0] = 0; 
+
+  for (let i = 0; i < vertexCount - 1; i++) {
+    const currentVertex = findClosestUnvisitedVertex(distances, visited);
+    visited[currentVertex] = true;
+
+    locations.forEach((location, index) => {
+      if (!visited[index]) {
+        const distance = calculateDistance(locations[currentVertex], location);
+        if (distance < distances[index]) {
+          distances[index] = distance;
+        }
+      }
+    });
+  }
+
+  return distances;
+}
+function iniciarViaje() {
+
+  
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 9,
+    center: coordsCostaRica ,  mapTypeId: google.maps.MapTypeId.ROADMAP, 
+  });
+
+  
+  locations.forEach((location) => {
+    new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+  });
+
+
+ // Calcular y mostrar la ruta más corta usando el algoritmo de Prim
+ const distances = primAlgorithm(locations);
+
+ let previousVertex = 0;
+ let totalDistance = 0;
+
+ for (let i = 1; i < locations.length; i++) {
+   totalDistance += distances[i];
+   const line = new google.maps.Polyline({
+     path: [locations[previousVertex], locations[i]],
+     strokeColor: '#FF0000',
+     strokeOpacity: 1.0,
+     strokeWeight: 2,
+     map: map,
+   });
+   previousVertex = i;
+ }
+
+
+
+
+
+}
+
+iniciarViaje();
