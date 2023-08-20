3f52f36e5aafbe6e95e75e21eb7f8dec00645759
isabellaaaaa03 <100324470+isabellaaaaa03@users.noreply.github.com>
2023-08-20 00:23:10 -0600

    prim new

--- prim.js
+++ prim.js
@@ -2,12 +2,16 @@ var viajes;
 //inputInicio = document.getElementById('inputInicio');
 //inputFinal = document.getElementById('inputFinal');
 //var nodos, aristas;
+// archivo2.js
+let service;
+let locations;
+let positionInicio;
 fetch("/datos-viajes")
   .then((response) => response.json())
   .then((data) => {
     viajes = data;
     console.log("Datos ", viajes);
-  
+
     arbol();
   })
   .catch((error) => {
@@ -15,16 +19,16 @@ fetch("/datos-viajes")
   });
 
 function arbol() {
-  const nodes = viajes.map((node) => ({ 
+  /*const nodes = viajes.map((node) => ({ 
     id: node.idViaje, 
-    name: node.nodoViajeInicio }));
+    name: node.nodoViajeInicio }));*/
   const edges = viajes.map((edge) => ({
     from: edge.nodoViajeInicio,
     to: edge.nodoViajeDestino,
     weight: edge.ponderado,
   }));
-
-  console.log("nodos", nodes);
+ 
+  //console.log("nodos", nodes);
   console.log("Aristas", edges);
 
   // Calcular el árbol de expansión mínima usando el algoritmo de Prim
@@ -62,12 +66,59 @@ function primAlgorithm(nodes, edges) {
   return selectedEdges;
 }
 */
+
+// Función para procesar las ubicaciones y obtener las coordenadas
+/*async function processLocations(locations) {
+  const locationsWithCoordinates = [];
+
+  for (const location of locations) {
+    const inicio = await geocodeAddress(location.inputInicio);
+    const final = await geocodeAddress(location.inputFinal);
+    
+    locationsWithCoordinates.push({
+      inputInicio: inicio,
+      inputFinal: final
+    });
+  }
+
+  return locationsWithCoordinates;
+}
+
+// Llamada a la función para obtener las coordenadas
+processLocations(locations)
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 // Funcion para calcular la distancia entre dos puntos utilizando la API de Google Maps
 function calculateDistance(position1, position2) {
   return google.maps.geometry.spherical.computeDistanceBetween(
     new google.maps.LatLng(position1.lat, position1.lng),
     new google.maps.LatLng(position2.lat, position2.lng)
   );
+}
+*/
+
+function obtenerLatLog(place) {
+
+  if (!place.geometry || !place.geometry.location) return;
+
+
+
+  positionInicio = {
+
+    lat: place.geometry.location.lat(),
+
+    lng: place.geometry.location.lng(),
+
+  };
+
+  console.log("posicion", positionInicio);
+
+  return positionInicio;
+
 }
 function findClosestUnvisitedVertex(distances, visited) {
   let minDistance = Number.MAX_VALUE;
@@ -93,7 +144,7 @@ function primAlgorithm(locations) {
     visited[index] = false;
   });
 
-  distances[0] = 0; 
+  distances[0] = 0;
 
   for (let i = 0; i < vertexCount - 1; i++) {
     const currentVertex = findClosestUnvisitedVertex(distances, visited);
@@ -115,46 +166,161 @@ function iniciarViaje() {
   const btnPopUp = document.querySelector('.btnViaje');
   //const wrapper = document.getElementById('wrapper'); // Agrega la referencia al elemento 'wrapper'
   const coordsCostaRica = { lat: 9.7489, lng: -83.7534 }; // Coordenadas de Costa Rica o cualquier otro centro de referencia
-  
-  btnPopUp.addEventListener('click', () => {
-    //wrapper.classList.add('active');
 
-    const locations = [{inputInicio,inputFinal}];
+  btnPopUp.addEventListener('click', () => {
+
+    let viajeDestino, viajeSiguienteInicio;
+
+    // console.log("Size", nodes.size);
+    const nodes = new Set();
+
+ 
+
+    viajes.forEach((edge) => {
+  
+      nodes.add(edge.from);
+  
+      nodes.add(edge.to);
+  
+    });
+
+    for (let n = 0; n < nodes.size; n++) {
+
+      if (viajes[n] == undefined) {
+
+        break;
+
+      }
+
+      //console.log( "Primero", viajes[n]);
+
+      for (let m = 0; m < nodes.size; m++) {
+
+        if (viajes[n] === viajes[m]) {
+
+          //console.log('igualdad');
+          m = n + 1;
+
+        }
+
+        if (viajes[m] == undefined) {
+
+          viajeSiguienteInicio = viajes[n].from
+
+          break;
+
+        }
+
+        viajeDestino = viajes[n].to;
+
+        viajeSiguienteInicio = viajes[m].from;
+
+        console.log("Inicio 1 ", viajeDestino, "Destino", viajeSiguienteInicio);
+
+        //console.log("Siguiente", viajes[m]);
+
+        const request1 = {
+
+          query: viajeDestino,
+
+          fields: ["name", "geometry"],
+
+        };
+
+        const request2 = {
+
+          query: viajeSiguienteInicio,
+
+          fields: ["name", "geometry"],
+
+        };
+
+
+
+
+
+        service = new google.maps.places.PlacesService(map);
+
+
+
+        service.findPlaceFromQuery(request1, (results, status) => {
+
+          if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+
+            for (let i = 0; i < results.length; i++) {
+
+              locations = (obtenerLatLog(place));
+
+            }
+
+
+
+
+
+          }
+
+        });
+
+        service.findPlaceFromQuery(request2, (results, status) => {
+
+          if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+
+            for (let i = 0; i < results.length; i++) {
+
+              locations = (obtenerLatLog(place));
+
+            }
+
+
+
+
+
+          }
+
+        });
+
+
+
+      }}
+
+      //const locations = [{obtenerLatLog(place)}];
+
       //nodoViajeInicio = document.getElementById('inputInicio').value;
       //const nodoViajeDestino = document.getElementById('inputFinal').value];
-  
-    const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 9,
-      center: coordsCostaRica,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-    });
 
-    locations.forEach((location) => {
-      new google.maps.Marker({
-        position: location,
-        map: map,
+      const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: coordsCostaRica,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
       });
-    });
 
-    // Calcular y mostrar la ruta más corta usando el algoritmo de Prim
-    const distances = primAlgorithm(locations);
-
-    let previousVertex = 0;
-    let totalDistance = 0;
-
-    for (let i = 1; i < locations.length; i++) {
-      totalDistance += distances[i];
-      console.log("Distancias",distances[i]);
-      const line = new google.maps.Polyline({
-        path: [locations[previousVertex], locations[i]],
-        strokeColor: '#FF0000',
-        strokeOpacity: 1.0,
-        strokeWeight: 2,
-        map: map,
+      locations.forEach((location) => {
+        new google.maps.Marker({
+          position: location,
+          map: map,
+        });
       });
-      previousVertex = i;
-    }
-  });
-}
+
+      // Calcular y mostrar la ruta más corta usando el algoritmo de Prim
+      const distances = primAlgorithm(locations);
+
+      let previousVertex = 0;
+      let totalDistance = 0;
+
+      for (let i = 1; i < locations.length; i++) {
+        totalDistance += distances[i];
+        console.log("Distancias", distances[i]);
+        const line = new google.maps.Polyline({
+          path: [locations[previousVertex], locations[i]],
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2,
+          map: map,
+        });
+        previousVertex = i;
+      }
+    });
+  }
+
 
 iniciarViaje();
